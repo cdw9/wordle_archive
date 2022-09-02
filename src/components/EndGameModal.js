102d13b57faccd65f932f3ae3f8ebeac5d4200f4
Chrissy Wainwright <chrissy@sixfeetup.com>
2022-09-02 13:41:25 -0500

    adjustments

--- src/components/EndGameModal.js
+++ src/components/EndGameModal.js
@@ -115,7 +115,7 @@ export const EndGameModal = ({
           {gameState === state.won && (
             <>
               <img src={Success} alt="success" height="auto" width="auto" />
-              <h1 className=" text-3xl">You are seriously the best!</h1>
+              <h1 className=" text-3xl">❤️ You are seriously the bestest!!!1!!<br/>#winning #wordlewinner #winnerwinnersushidinner</h1>
               <p className="mt-3 text-2xl">
                 Won: {wins}
               </p>
@@ -128,7 +128,7 @@ export const EndGameModal = ({
             <>
               <img src={Fail} alt="success" height="auto" width="80%" />
               <div className="text-primary dark:text-primary-dark text-4xl text-center">
-                <p>Oops!</p>
+                <p>Oh no! 🧇 😢</p>
                 <p className="mt-3 text-2xl">
                   The word was <strong>{answer}</strong>
                 </p>
